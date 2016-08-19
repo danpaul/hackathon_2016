6f40b640322b5f1233a7cdc07930e0e7d1aa2332
Adrien Goulet <ace@sanbornmediafactory.com>
2016-08-19 13:31:25 -0700

    stylesheet link

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	<link rel="apple-touch-icon" sizes="114x114" href="img/appletouch-114x114.jpg" />
 	<link rel="apple-touch-icon" sizes="144x144" href="img/appletouch-144x144.jpg" />
 	<link rel="stylesheet" href="twemoji-awesome.css">
-	<link rel="stylesheet" href="twemoji-awesome.css?v=<?php echo round(microtime(true) * 1000); ?>">
+	<link rel="stylesheet" href="style.css?v=<?php echo round(microtime(true) * 1000); ?>">
 	<script type='text/javascript' src='js/lib/jquery.1.9.1.min.js'></script>
 </head>
 
