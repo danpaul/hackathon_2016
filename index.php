23da9021151332a4110287f4053cff97d0377496
Adrien Goulet <ace@sanbornmediafactory.com>
2016-08-19 08:19:38 -0700

    basic front end

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 7]><html class="lt-ie9 lt-ie8" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 8]><html class="lt-ie9" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="en" class="gt-ie9" xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Jen B</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+	<link rel="shortcut icon" href="img/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="114x114" href="img/appletouch-114x114.jpg" />
+	<link rel="apple-touch-icon" sizes="144x144" href="img/appletouch-144x144.jpg" />
+	<link rel='stylesheet' href='style.css' type='text/css' media='all' />
+	<script type='text/javascript' src='js/lib/jquery.1.9.1.min.js'></script>
+</head>
+
+<body>
+    
+    <script type='text/javascript' src='js/vendor/jquery.form.js'></script>
+    <script type='text/javascript' src='js/vendor/jquery.validate.min.js'></script>
+    <script type='text/javascript' src='js/scripts.js'></script>
+</body>
+
+</html>
