501c4e3aa97c3abbcb822ddb81fbd1cb3ef0c431
Adrien Goulet <ace@sanbornmediafactory.com>
2016-08-19 08:27:54 -0700

    adding emoji library

--- index.php
+++ index.php
@@ -12,11 +12,14 @@
 	<link rel="shortcut icon" href="img/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="114x114" href="img/appletouch-114x114.jpg" />
 	<link rel="apple-touch-icon" sizes="144x144" href="img/appletouch-144x144.jpg" />
+	<link rel="stylesheet" href="twemoji-awesome.css">
 	<link rel='stylesheet' href='style.css' type='text/css' media='all' />
 	<script type='text/javascript' src='js/lib/jquery.1.9.1.min.js'></script>
 </head>
 
 <body>
+    
+    
     
     <script type='text/javascript' src='js/vendor/jquery.form.js'></script>
     <script type='text/javascript' src='js/vendor/jquery.validate.min.js'></script>
